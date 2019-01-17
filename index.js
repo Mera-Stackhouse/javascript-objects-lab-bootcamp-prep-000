1edd8632eed707dd2faf5c6c83a50476acfe4e55
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 04:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var recipes = {
   salt: '1/4 ts'
 }
 
-function updateObjectWithKeyAndValue(object,key,value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
 }
