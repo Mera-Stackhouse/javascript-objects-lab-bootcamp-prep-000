3e92d5de0ac256e3e5c80169d3e2209e700b90bf
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 04:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = { 
-  vegan mayo: '2/3 cups', 
-  garlic cloves: '2',
-  lemon juice: '1 ts',
+  [vegan mayo]: '2/3 cups', 
+  [garlic cloves]: '2',
+  [lemon juice]: '1 ts',
   salt: '1/4 ts'
 }
 
